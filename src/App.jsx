5853f149c5421135ebbfa12ec79d3c15e2c3b0f4
sukaina-s <sukainasunil@gmail.com>
2025-08-19 17:05:40 +0400

    fixed build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/sidebar/sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main.jsx'
 
 const App = () => {
